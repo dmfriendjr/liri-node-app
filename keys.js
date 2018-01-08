ea67c5cdbac2b79e5929e4382652f360acc6a241
dmfriendjr <davidmfriendjr@gmail.com>
2018-01-08 15:13:57 -0500

    Added serachign by track name on spotify, along with selecting the desired song from the results list

--- keys.js
+++ keys.js
@@ -1,5 +1,3 @@
-console.log('API Keys Loaded');
-
 exports.twitter = {
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
 	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -11,4 +9,3 @@ exports.spotify = {
 	id: process.env.SPOTIFY_ID,
 	secret: process.env.SPOTIFY_SECRET
 };
-
--- liri.js
+++ liri.js
@@ -14,6 +14,7 @@ switch(userCommand) {
 		getLastTweets();
 		break;
 	case 'spotify-this-song':
+		getSongInfo(getUserInput());
 		break;
 	case 'movie-this':
 		break;
@@ -22,18 +23,58 @@ switch(userCommand) {
 }
 
 function getLastTweets() {
-
-
-	twitterClient.get('statuses/home_timeline', function(error, tweets, response) {
+	twitterClient.get('statuses/home_timeline', { tweet_mode: 'extended' }, function(error, tweets, response) {
 		if (error) {
 			return console.log(error);
 		}
-	
+		console.log(`********************Twitter********************`);
+
 		tweets.forEach( (tweet) => {
+			console.log(tweet.user.screen_name);
 			console.log(tweet.created_at);
-			console.log(tweet.text);
+			if (tweet.retweeted_status) {
+				console.log(tweet.retweeted_status.full_text);
+			} else {
+				console.log(tweet.full_text);
+			}
 			console.log('-----------');
 		});
 	});
+}
 
+function getSongInfo(song) {
+	if (song.length === 0) song = 'The Sign';
+
+	spotifyClient.search({ type: 'track', query: song }, function(err, response) {
+		if (err) return console.log(err);
+
+		console.log(song);
+		console.log('-----Song Results-----');
+		console.log(response.tracks.items[0].name);
+		response.tracks.items.forEach( (result, index) => {
+			//console.log(result);
+			console.log(`${index}) ${result.name} by ${result.artists[0].name}`);
+		});
+
+		console.log('Please enter number of desired track');
+		let selection = process.stdin.read();
+		process.stdin.resume();
+		process.stdout.write('Please enter number of desired track');
+		process.stdin.once("data", function(data) {
+			let displayTrack = response.tracks.items[parseFloat(data)];
+			console.log(`${displayTrack.name} by ${displayTrack.artists[0].name}`);
+			console.log(displayTrack.album.name);
+			process.stdin.pause();
+		});
+	});
+}
+
+function getUserInput() {
+	let input = '';
+
+	for (let i = 3; i < process.argv.length; i++) {
+		input += ' ' + process.argv[i];	
+	}
+
+	return input;
 }
