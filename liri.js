853e441ef93dd5e281ccbb9657a808cf66a23912
dmfriendjr <davidmfriendjr@gmail.com>
2018-01-08 14:12:12 -0500

    Authenticating with twitter, retrieving tweets, and displaying last 20 tweets in console

--- liri.js
+++ liri.js
@@ -1,6 +1,39 @@
 require("dotenv").config();
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
 let apiKeys = require('./keys');
 
-let spotify = new Spotify(apiKeys.spotify);
-let client = new Twitter(apiKeys.twitter);
+let spotifyClient = new Spotify(apiKeys.spotify);
+let twitterClient = new Twitter(apiKeys.twitter);
+
+let userCommand = process.argv[2];
+
+switch(userCommand) {
+	case 'my-tweets':
+		getLastTweets();
+		break;
+	case 'spotify-this-song':
+		break;
+	case 'movie-this':
+		break;
+	case 'do-what-it-says':
+		break;
+}
+
+function getLastTweets() {
+
+
+	twitterClient.get('statuses/home_timeline', function(error, tweets, response) {
+		if (error) {
+			return console.log(error);
+		}
+	
+		tweets.forEach( (tweet) => {
+			console.log(tweet.created_at);
+			console.log(tweet.text);
+			console.log('-----------');
+		});
+	});
+
+}
