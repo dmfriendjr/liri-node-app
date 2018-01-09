e648726adc62f11c9601f6a191603dca703f475c
dmfriendjr <davidmfriendjr@gmail.com>
2018-01-09 12:41:49 -0500

    Added pulling commands from a file with support for multiple commands on seperate lines

--- liri.js
+++ liri.js
@@ -3,48 +3,81 @@ let Twitter = require('twitter');
 let Spotify = require('node-spotify-api');
 let request = require('request');
 let fs = require('fs');
+let readline = require('readline');
 let apiKeys = require('./keys');
 let date = new Date();
+
+let lineReader = readline.createInterface({
+	input: fs.createReadStream('random.txt')
+});
 
 let spotifyClient = new Spotify(apiKeys.spotify);
 let twitterClient = new Twitter(apiKeys.twitter);
 
 let userCommand = process.argv[2];
 
-switch(userCommand) {
-	case 'my-tweets':
-		getLastTweets();
-		break;
-	case 'spotify-this-song':
-		getSongInfo(getUserInput());
-		break;
-	case 'movie-this':
-		getMovieInfo(getUserInput());
-		break;
-	case 'do-what-it-says':
-		break;
+processUserCommand(userCommand, getUserInput());
+
+function processUserCommand(command, input) {
+	switch(command) {
+		case 'my-tweets':
+			getLastTweets();
+			break;
+		case 'spotify-this-song':
+			getSongInfo(input);
+			break;
+		case 'movie-this':
+			getMovieInfo(input);
+			break;
+		case 'do-what-it-says':
+			readCommandFile();
+			break;
+	}
 }
 
 function getMovieInfo(search) {
 	if (search.length === 0) search = 'Mr. Nobody';
-	
-	request(`http://www.omdbapi.com/?apikey=${apiKeys.omdb.key}&t=${search}`, function (err, response, body) {
+
+	let queryType = 't';
+	let queryString;
+	console.log(Array.isArray(search));
+	if (Array.isArray(search)) {
+		//Flags have been passed
+		search.forEach( (flag) => {
+			if (flag.charAt(0) === '-') {
+				//This is a flag
+				if (flag === '-s') {
+					queryType = 's';
+				}
+			} else {
+				//This is not a flag, so must be the search string
+				queryString = flag;
+			}
+		});		
+	} else {
+		queryString = search;
+	}
+
+	console.log(queryString);
+	request(`http://www.omdbapi.com/?apikey=${apiKeys.omdb.key}&${queryType}=${queryString}`, function (err, response, body) {
 		if (err) {
 			return writeToLog(err);
 		}
-
 		body = JSON.parse(body);
-		let logString = ``;
-		logString += `Title: ${body.Title}\n`;
-		logString += `Release Year: ${body.Year}\n`;
-		body.Ratings.forEach( (rating) => {
-			logString += `${rating.Source}: ${rating.Value}\n`;
-		});
-		logString += `Country: ${body.Country}\n`;
-		logString += `Language: ${body.Language}\n`;
-		logString += `Plot: ${body.Plot}\n`
-		logString += `Actors: ${body.Actors}\n`;
-		writeToLog(logString);
+		console.log(body);
+		if (queryType === 't') {
+			let logString = ``;
+			logString += `Title: ${body.Title}\n`;
+			logString += `Release Year: ${body.Year}\n`;
+			body.Ratings.forEach( (rating) => {
+				logString += `${rating.Source}: ${rating.Value}\n`;
+			});
+			logString += `Country: ${body.Country}\n`;
+			logString += `Language: ${body.Language}\n`;
+			logString += `Plot: ${body.Plot}\n`
+			logString += `Actors: ${body.Actors}\n`;
+			writeToLog(logString);
+		}
 	});
 }
 
@@ -75,7 +108,7 @@ function getSongInfo(song) {
 	if (song.length === 0) song = 'The Sign';
 
 	spotifyClient.search({ type: 'track', query: song }, function(err, response) {
-		if (err) return console.log(err);
+		if (err) return writeToLog(err);
 
 		console.log('-----Song Results-----');
 
@@ -98,14 +131,40 @@ function getSongInfo(song) {
 	});
 }
 
+function readCommandFile() {
+	lineReader.on('line', function(line) {
+		let fileCommand = line.split(' ');
+		let command = fileCommand[0];
+		console.log('Processing:', command);
+		let input = '';
+		for (let i = 1; i < fileCommand.length; i++) {
+			input += fileCommand[i] + ' ';
+		}
+
+		input.trim();
+		processUserCommand(command, input);
+	});
+}
+
 function getUserInput() {
 	let input = '';
+	let flags = [];
 
 	for (let i = 3; i < process.argv.length; i++) {
-		input += ' ' + process.argv[i];	
+		console.log(`argv:${process.argv[i]}`);
+		if (process.argv[i].charAt(0) === '-') {
+			flags.push(process.argv[i]);
+		} else {
+		input += process.argv[i] + ' ';	
+		}
 	}
-
-	return input;
+	
+	if (flags.length !== 0) {
+		flags.push(input.trim());
+		return flags;
+	} else {
+		return input;	
+	}
 }
 
 function writeToLog(string) {
